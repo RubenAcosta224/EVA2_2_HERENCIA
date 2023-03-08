ff8aedfd40900b01c2aa44ebd87ec033b84839f4
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-03-08 17:54:00 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Docente.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_2_herencia;
+
+/**
+ *
+ * @author invitado
+ */
+public class Docente {
+    private String nombre;
+    private String apellido;
+    private String plaza;
+
+    
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPlaza() {
+        return plaza;
+    }
+
+    public void setPlaza(String plaza) {
+        this.plaza = plaza;
+    }
+    
+    
+}
---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Estudiante.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_2_herencia;
+
+/**
+ *
+ * @author invitado
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int noControl;
+
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(int noControl) {
+        this.noControl = noControl;
+    }
+    
+    
+    
+    
+}
